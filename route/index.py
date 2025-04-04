edc750187ccad9d123269084e1de96e4668a6903
syh053 <syh053@gmail.com>
2025-04-04 21:15:54 +0800

    wrong address will back to previous page

--- route/index.py
+++ route/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from .stock_news import stock_news 
 from .stock_analysis import analysis
 
@@ -14,3 +14,9 @@ routes.register_blueprint(analysis, url_prefix = "/stock")
 @routes.route("/index", methods = ["GET"])
 def index():
   return render_template("index.html")
+
+
+# 404 錯誤處理：導回首頁
+@routes.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404  # 返回 404.html 並設定狀態碼r('routes.index'))
